389086d6334494b8a0b35661f2f1623e4ee7b2f1
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2019-11-29 00:25:56 +0100

    Added connect and disconnect event handles

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
+const http = require('http');
+
 const express = require('express');
+
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public/');
 
@@ -11,6 +15,19 @@ const app = express();
 //creates a middleware to server files from with a given root directory.
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+//lets you register an event
+io.on('connection', (socket) => {
+    console.log("New user connected");
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+    })
+});
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
