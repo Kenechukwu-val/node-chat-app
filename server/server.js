3f619f2fba501816869d336e0bdba186d4886904
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2019-12-01 17:05:41 +0100

    Added generatedMessage Utility

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public/');
 
+const {generateMessage} = require('./utils/message');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -22,6 +24,26 @@ const io = socketIO(server);
 //lets you register an event
 io.on('connection', (socket) => {
     console.log("New user connected");
+
+    
+    socket.emit('newMsg', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newMsg', generateMessage('Users', 'New user joined'));
+
+    socket.on('createMsg', (message) => {
+        console.log('createMsg', message)
+        //Makes the message sent visible to everyone connected to the server
+        io.emit('newMsg', generateMessage(message.from, message.text));
+
+        //sends a message to all connected user but doesn't receive the msg
+        // socket.broadcast.emit('newMsg', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAT: new Date().getTime()
+        // })
+
+
+    })
 
     socket.on('disconnect', () => {
         console.log("Client disconnected");
